79911e2e73e8f82612030e58552b15d275ebec5b
itay601 <itay601@LAPTOP-UI69H6FF>
2024-06-11 11:17:07 +0300

    A.2 - first steps in jetpack compose

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,6 +33,13 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures {
+        compose = true
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.13"
+    }
 }
 
 dependencies {
@@ -43,7 +50,58 @@ dependencies {
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.core)
+    implementation(libs.androidx.ui.tooling.preview.android)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    val composeBom = platform("androidx.compose:compose-bom:2024.05.00")
+    implementation(composeBom)
+    androidTestImplementation(composeBom)
+
+    // Choose one of the following:
+    // Material Design 3
+    //noinspection UseTomlInstead
+    implementation("androidx.compose.material3:material3")
+    // or Material Design 2
+    //noinspection UseTomlInstead
+    implementation("androidx.compose.material:material")
+    // or skip Material Design and build directly on top of foundational components
+    //noinspection UseTomlInstead
+    implementation("androidx.compose.foundation:foundation")
+    // or only import the main APIs for the underlying toolkit systems,
+    // such as input and measurement/layout
+    //noinspection UseTomlInstead
+    implementation("androidx.compose.ui:ui")
+
+    // Android Studio Preview support
+    //noinspection UseTomlInstead
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    //noinspection UseTomlInstead
+    debugImplementation("androidx.compose.ui:ui-tooling")
+
+    // UI Tests
+    //noinspection UseTomlInstead
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+    //noinspection UseTomlInstead
+    debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+    // Optional - Included automatically by material, only add when you need
+    // the icons but not the material library (e.g. when using Material3 or a
+    // custom design system based on Foundation)
+    //noinspection UseTomlInstead
+    implementation("androidx.compose.material:material-icons-core")
+    // Optional - Add full set of material icons
+    //noinspection UseTomlInstead
+    implementation("androidx.compose.material:material-icons-extended")
+    // Optional - Add window size utils
+    //noinspection UseTomlInstead
+    implementation("androidx.compose.material3:material3-window-size-class")
+
+
+       //noinspection UseTomlInstead
+       implementation("androidx.compose.runtime:runtime-livedata")
+    // Optional - Integration with RxJava
+    //noinspection UseTomlInstead
+    implementation("androidx.compose.runtime:runtime-rxjava2")
 }
--- app/src/main/java/com/example/mykotlinproject/MainActivity.kt
+++ app/src/main/java/com/example/mykotlinproject/MainActivity.kt
@@ -1,15 +1,41 @@
 package com.example.mykotlinproject
 
 import android.os.Bundle
-//import androidx.activity.enableEdgeToEdge
+import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-//import androidx.core.view.ViewCompat
-//import androidx.core.view.WindowInsetsCompat
+import androidx.compose.foundation.layout.fillMaxSize
+//noinspection UsingMaterialAndMaterial3Libraries
+import androidx.compose.material.Surface
+//noinspection UsingMaterialAndMaterial3Libraries
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.core.content.pm.ShortcutInfoCompat.Surface
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+
+
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+                            // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    Greeting("Android")
+                }
 
+        }
     }
+}
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
 }
